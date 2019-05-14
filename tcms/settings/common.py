846fe943e7dcd60f77300b9370b15abb18177486
fresnel-bruno <bruno@fresnel.cc>
2019-05-14 11:25:14 -0300

    Revert "settings"
    This reverts commit 56c449e9

--- tcms/settings/common.py
+++ tcms/settings/common.py
@@ -118,7 +118,7 @@ HELP_MENU_ITEMS = [
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '~/Kiwi/static/'
 
 
 # WARNING: Do not change this unless you know what you are doing !!!
@@ -193,7 +193,7 @@ USE_TZ = False
 TIME_ZONE = 'Etc/UTC'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/uploads'
+MEDIA_ROOT = '/Kiwi/uploads'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
