751c080d4f31c78bdf17ba394ff867c6f91cfe29
fresnel-bruno <bruno@fresnel.cc>
2019-05-14 11:25:25 -0300

    Revert "settings"
    This reverts commit 2a1a9cdd

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tcms.settings.product")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tcms.settings.devel")
 
     from django.core.management import execute_from_command_line
 
